f9791f54806c926e6d9f77c5a055f8c215dd415a
zalvapasha <zalvapasha1@gmail.com>
2024-11-09 15:08:42 +0700

    feat: setup project

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ChartNew } from "@/components/ChartNew";
+
+export default function Home() {
+  return (
+    <div className="flex items-center h-screen justify-center font-poppins">
+      <ChartNew />
+    </div>
+  );
+}
