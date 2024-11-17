54a37d2766414efcce5178cacdfaeae6a6e26ab5
zalvapasha <zalvapasha1@gmail.com>
2024-11-18 04:55:29 +0700

    feat: search, table, and new interfaces

--- client/src/app/interfaces/global.d.ts
+++ client/src/app/interfaces/global.d.ts
@@ -10,3 +10,25 @@ interface NewsItem {
   banner_image: string;
   ticker_sentiment?: { ticker: string; ticker_sentiment_label: string }[];
 }
+
+interface Ticker {
+  ticker: string;
+  name: string;
+  market: string;
+  locale: string;
+  active: boolean;
+  currency_symbol: string;
+  currency_name: string;
+  base_currency_symbol: string;
+  base_currency_name: string;
+  last_updated_utc: string;
+}
+
+interface ApiResponse {
+  results: Ticker[];
+  next_url?: string;
+}
+
+interface SearchResponse {
+  results: SearchResponseItem[];
+}
